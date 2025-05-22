df6cb8f8807747585c39d9953c3242a2e886efcc
Mohit <mohit@geoiq.io>
2025-05-22 13:34:42 +0530

    changed

--- Sources/geoiq-vision-ios-sdk/geoiq_vision_ios_sdk.swift
+++ Sources/geoiq-vision-ios-sdk/geoiq_vision_ios_sdk.swift
@@ -1,4 +1,3 @@
-
 @_exported import LiveKit
 import Foundation
 import Combine
@@ -21,12 +20,12 @@ public enum GeoVisionEvent {
     case participantAttributesChanged(participant: Participant, metadata: String)
 }
 
-public class VisionBotSDKMananger: NSObject, RoomDelegate, ParticipantDelegate {
+open class VisionBotSDKMananger: NSObject, RoomDelegate, ParticipantDelegate {
 
     public let eventPublisher = PassthroughSubject<GeoVisionEvent, Never>()
     private(set) public var room: Room?
 
-    override init() {
+    public override init() {
         self.room = Room()
         super.init()
         room?.delegates.add(delegate: self)
